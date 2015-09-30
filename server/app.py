5a25c869ba569895756ffbe8cf2336021f8c180e
suhithr <suhithrajesh@yahoo.com>
2015-09-30 16:31:12 +0530

    Started db schema

--- server/app.py
+++ server/app.py
@@ -1,13 +1,18 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, request
 from flask.ext.sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config.from_object('config.DevelopmentConfig')
 
+#Creating database object
 db = SQLAlchemy(app)
 
 
-
+@app.route('/visited', methods=['GET', 'POST'])
+def visited():
+	if request.method == 'GET':
+		visited_url = request.args['page']
 
 
 if __name__ == '__main__':
