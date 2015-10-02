a41913026573434f0a96fbfb55e4287495c08114
Parth Thakkar <thakkarparth007@gmail.com>
2015-10-02 22:12:46 +0530

    Fixed dealing with FB API

--- extension/popup.js
+++ extension/popup.js
@@ -1,24 +1,25 @@
 var wait = 2;
 
 function work() {
-	chrome.runtime.sendMessage({directive: "popup-click"}, function(response) {
-		console.log("Lulllllllz");
-	});
+	//chrome.runtime.sendMessage({directive: "popup-click"}, function(response) {
+	//	console.log("Lulllllllz");
+	//});
 	document.getElementById("box").addEventListener('click',function(){
 		document.getElementById("box").innerHTML = "black";
-		chrome.runtime.sendMessage({directive: "popup-click"}, function(response) {
-			//this.close(); // close the popup when the background finishes processing request
-		});
+	//	chrome.runtime.sendMessage({directive: "popup-click"}, function(response) {
+	//		//this.close(); // close the popup when the background finishes processing request
+	//	});
 
 		FB.getLoginStatus(function(response) {
 			if (response.status === 'connected') {
-				console.log('Logged in.');
+				alert('Logged in.');
 			}
 			else {
+				alert("not logged in");
 				FB.login(function(){
-			  // Note: The call will only work if you accept the permission request
-			  FB.api('/me/feed', 'post', {message: 'Hello, world!'});
-			}, {scope: 'publish_actions'});
+				  // Note: The call will only work if you accept the permission request
+				  FB.api('/me/feed', 'post', {message: 'Hello, world!'});
+				}, {scope: 'public_profile,email,user_friends'});
 			}
 		});
 	});
@@ -29,21 +30,16 @@ window.addEventListener('DOMContentLoaded', function() {
 	if(!wait) work();
 });
 
+FB.loadAPI('https://thakkarparth007.github.io/');
+
 window.fbAsyncInit = function() {
 	FB.init({
 		appId      : '964263926953204',
-		xfbml      : false,
-		version    : 'v2.4'
+		xfbml      : true,
+		version    : 'v2.4',
+		oauth      : true
 	});
 
 	wait--;
 	if(!wait) work();
 };
-
-(function(d, s, id){
-	var js, fjs = d.getElementsByTagName(s)[0];
-	if (d.getElementById(id)) {return;}
-	js = d.createElement(s); js.id = id;
-	js.src = "https://connect.facebook.net/en_US/sdk.js";
-	fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
